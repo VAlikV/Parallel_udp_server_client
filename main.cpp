d7f498d738ba4ea0334600be0e84a34280d2dd90
ValikV <alik.valiullin2002@yandex.ru>
2025-05-03 13:03:29 +0300

    replace to template for eigen

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@
 
 int main()
 {
-    int a;
+    int a; 
+    Eigen::Array<double,7,1> b;
+    b << 1, 2, 3, 4, 5, 6, 7;
 
-    server::UDPServer server("127.0.0.1", 8084, "127.0.0.1", 8081);
+    server::UDPServer<7,7> server("127.0.0.1", 8084, "127.0.0.1", 8081);
 
     server.start();
 
     while(1)
     {
         std::cin >> a; 
-        server.setMsg();
+        server.setMsg(b);
     }
 
     server.stop();
